f83a9000a95e89196a636fa8ce418590795fe55d
corymcan <rulerfenrir@aim.com>
2015-02-16 11:36:57 -0500

    fixed output, need to update callbacks

--- Code/consolidateAllAuditFilesInProgress.js
+++ Code/consolidateAllAuditFilesInProgress.js
@@ -5,28 +5,42 @@ var fs = require('fs');
 var s3 = new aws.S3({apiVersion: '2006-03-01'});
 
 exports.consolidateAudit = function(event, context) {
+   console.log("run");
    s3.listObjects({Bucket:'senior-projects-cory'},
       function(err,data) {
         if (err) {
-           console.log('error listing objects from bucket ');
+           console.log('error listing objects from bucket ' + bucket);
            context.done('error',err);
         }
         else {
            console.log('data: ', data);
+           //context.done(null,'done');
         }
       }
    );
 
+
+var currentFile = fs.readFileSync('senior-projects-cory/helloworld.txt','utf8')
+console.log ("print txt! : " + currentFile);
+
+
 function processFile(callback){
     fs.readFile(currentFile, function fileRead(err, fileContents){
         var contents = new Buffer(currentFile.size);
+        
+        
     }
-}
+)}
+    
 
+
+//not working yet
+/*
 s3.getObject({Bucket:'senior-projects-cory', Key: 'helloworld.txt'}, function gotObject(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 });
+*/
 
 context.done(null, "done consolidating");
 }//end
